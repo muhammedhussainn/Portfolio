4f0cde32020fabc054a2664e8003d11fa7400731
muhammedhussainn <l7creationss@gmail.com>
2024-12-28 21:40:11 +0530

    Update SocialIcons.js

--- src/subComponents/SocialIcons.js
+++ src/subComponents/SocialIcons.js
@@ -39,7 +39,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://github.com/codebucks27"}
+          href={"https://github.com/MdHussain02"}
         >
           <Github
             width={25}
@@ -56,7 +56,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://twitter.com/code_bucks"}
+          href={"https://x.com/Muhamma52259744"}
         >
           <Twitter
             width={25}
@@ -73,7 +73,7 @@ const SocialIcons = (props) => {
         <a
           style={{ color: "inherit" }}
           target="_blank"
-          href={"https://facebook.com/codebucks27"}
+          href={"https://www.facebook.com/Muhammad.2002.hussain"}
         >
           <Facebook
             width={25}
